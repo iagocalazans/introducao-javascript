1188f8824eeb3e55594696b628e8e35ca56ef1ed
Iago Calazans <iago.calazans@gmail.com>
2019-11-15 04:09:41 -0300

    Funçao limpar histórico
    Inserção de nova função para limpar o histórico de cálculos realizados.

--- js/principal.js
+++ js/principal.js
@@ -1,7 +1,17 @@
-var button = document.querySelector("#adicionar-paciente");
+var adicionar = document.querySelector("#adicionar-paciente");
+var limpar = document.querySelector("#limpar-historico");
 
-button.addEventListener("click", function(event) {
+adicionar.addEventListener("click", function(event) {
     event.preventDefault();
     var paciente = new Paciente();
     paciente.executa();
+});
+
+limpar.addEventListener("click", function(event) {
+    event.preventDefault();
+
+    var calculos = document.querySelectorAll(".paciente");
+    for (i=0; i < calculos.length; i++) {
+        calculos[i].remove();
+    }
 });
